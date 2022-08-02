4c676cfe3db969dc78945cc0fa426d40ad2f483d
Ivan6572 <92355813+Ivan6572@users.noreply.github.com>
2022-08-02 16:36:36 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,15 @@
 function factorial(n) {
-  if (n < 0) return;
-  if (n < 2) return 1;
-  return n * factorial(n - 1);
+    if(n > 0)
+      
+        } else {
+          
+        let result = 1;
+        for (let i = n; i == 1; i--) {
+        
+            result *= i;
+        }
+
+        return result;
+    }
 }
+
