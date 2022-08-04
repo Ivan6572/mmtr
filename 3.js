1c0d4b644c93ca7638d4dd938a4a6572818d93ce
Ivan6572 <92355813+Ivan6572@users.noreply.github.com>
2022-08-04 17:27:17 +0300

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,20 @@
+function largestOf(arr) {
+  var max;
+  var largestArray = [];
+
+  for (var i = 0; i < arr.length; i++) { // длина arr - это количество подмассивов
+    max = -Infinity; // max должен быть повторно инициализирован до наименьшего числа в каждом цикле
+    for (var j = 0; j < arr[i].length; j++) { // длина - это количество элементов в подмассиве
+      if (arr[i][j] > max) { // просто обновляет max до большего числа
+        max = arr[i][j];
+      }
+    }
+    
+    largestArray.push(max); // нажмите max после завершения внутреннего цикла, и max известен
+  }
+
+  return largestArray; // возвращает самый большой массив
+
+var result = largestOf([[4, 5, 1, 13], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+console.log(result);
